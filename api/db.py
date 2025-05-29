5f7aca17e7016dc0eac3cab3d53902b1c37b2984
James Sukanto <james_sukanto@aiap.sg>
2025-05-29 16:55:16 +0800

    setup web service

--- api/db.py
+++ api/db.py
@@ -2,9 +2,9 @@ import os
 from databases import Database
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv(override=True) 
+# load_dotenv(override=True) 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 print(f"\napi/db, url: {DATABASE_URL}\n")
--- throw/throw.md
+++ throw/throw.md
@@ -1,5 +1,5 @@
 # LOCAL
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000
 curl http://localhost:8000/ping
 
 # CREATE DB
