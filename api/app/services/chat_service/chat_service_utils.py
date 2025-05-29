6dcafb494c02c7fe83ad83833704b8f33206a95e
James Sukanto <james_sukanto@aiap.sg>
2025-05-29 20:20:35 +0800

    Debug tryout

--- api/app/services/chat_service/chat_service_utils.py
+++ api/app/services/chat_service/chat_service_utils.py
@@ -8,7 +8,7 @@ def apply_session_variables_with_engine(engine, user: str, fleet_id: str):
     """
     with engine.connect() as con:
         con.execute(text("SET statement_timeout = 10000;"))
-        con.execute(text(f"SET ROLE '{user}';"))
+        # con.execute(text(f"SET ROLE '{user}';"))
         con.execute(text(f"SET app.fleet_id = '{fleet_id}';"))
 
 
